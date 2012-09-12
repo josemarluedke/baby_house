d9b1ff0edf8914670ee98b306c476a732cb36fb9
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-12 09:43:30 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyHouse::Application.config.secret_token = 'bf8f61e4232d3f27e7b2ebf893a7ad6bf6fb2cb6d3b128643c6b2343b652522714552ae9e32e8c577ce320bbb849d97f2d4ce010bf401adcfb1cd1ddc3f42a76'
