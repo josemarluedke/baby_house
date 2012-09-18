f327350e6c14edc477c634b6eec0d581b839e5be
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-18 16:34:00 -0300

    removed the page active admin (for migrate on heroku)

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,28 +1,28 @@
-ActiveAdmin.register Page do
-  filter :title
-  filter :body
-  filter :created_at
-  filter :updated_at
+#ActiveAdmin.register Page do
+  #filter :title
+  #filter :body
+  #filter :created_at
+  #filter :updated_at
 
-  index do
-    column :title
-    column :slug
-    column :created_at
-    column :updated_at
-    default_actions
-  end
+  #index do
+    #column :title
+    #column :slug
+    #column :created_at
+    #column :updated_at
+    #default_actions
+  #end
 
-  form do |f|
-    f.inputs do
-      if f.object.persisted?
-        f.input :title
-        f.input :body
-        f.input :slug
-      else
-        f.input :title
-        f.input :body
-      end
-    end
-    f.buttons
-  end
-end
+  #form do |f|
+    #f.inputs do
+      #if f.object.persisted?
+        #f.input :title
+        #f.input :body
+        #f.input :slug
+      #else
+        #f.input :title
+        #f.input :body
+      #end
+    #end
+    #f.buttons
+  #end
+#end
