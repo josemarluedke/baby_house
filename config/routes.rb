59dcdcb999c23eb316e498146a81e46d7405dfc3
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-18 17:01:09 -0300

    added the show for page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ BabyHouse::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  resources :pages, only: [:index, :show]
   root :to => "pages#index"
+
+  # Pages (have to be in the end of file)
+  get '/:id', to: 'pages#show', as: :page
 end
