43a079e95a155e69d03817b5be133b7503b012d2
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-29 10:34:37 -0300

    removed the auto_html from page

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,14 +7,6 @@ class Page < ActiveRecord::Base
   validates :title, uniqueness: true
   belongs_to :category
 
-  auto_html_for :body do
-    html_escape
-    image
-    youtube
-    vimeo
-    redcarpet
-  end
-
   def should_generate_new_friendly_id?
     new_record?
   end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -15,18 +15,6 @@ describe Page do
     end
   end
 
-  describe "#auto_html" do
-    text = "A _simple_ body of *page* [This link](http://example.net/)"
-    let(:page) { Page.make!(:body => text) }
-
-    it "should have the right body" do
-      page.body.should == "A _simple_ body of *page* [This link](http://example.net/)"
-    end
-
-    it "should have the right body_html" do
-      page.body_html.should == "<p>A <em>simple</em> body of <em>page</em> <a href=\"http://example.net/\">This link</a></p>\n"
-    end
-  end
   describe "Associations" do
     it { should belong_to :category }
   end
