eb9b1a4b3e1fd5ee65d6bcdcffc224fcff61b18e
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-10-31 14:45:38 -0200

    organizing the tinymce editor

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -9,13 +9,11 @@ $(function () {
   tinymce.init({
     mode : "textareas",
     theme : "advanced",
-    plugins : "autolink,lists,style,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,contextmenu,paste,fullscreen,xhtmlxtras",
+    plugins : "autolink,lists,style,table,advimage,advlink,iespell,inlinepopups,contextmenu,paste",
     editor_selector: 'mceEditor',
 
     // Theme options
-    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,|,forecolor,backcolor",
-    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,,ltr,rtl,|,fullscreen",
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect,|,bullist,numlist,outdent,indent,|,link,unlink,image,|,table,pasteword,code",
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
