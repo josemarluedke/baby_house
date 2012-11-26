3cb0e072da45386b72afe969980fa842d3d5d669
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-11-26 15:12:02 -0200

    applied the right in activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,16 @@
+# Encoding: utf-8
 class ActivitiesController < ApplicationController
   inherit_resources
   actions :index, :show
   before_filter :authenticate_parent!
 
   def show
-  	return redirect_to root_url if can_see_activity(current_parent)
+  	return redirect_to(root_url, :alert => "Você não tem direito para ver esta atividade.") unless resource.can_see_activity(current_parent)
   	super
   end
+
+  protected
+    def collection
+      @activities ||= Activity.order('created_at DESC').parent_activities(current_parent)
+    end
 end
