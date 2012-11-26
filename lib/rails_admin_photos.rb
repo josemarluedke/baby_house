c2a1dd0733756b5b381f92736f7ded79226b0741
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-11-26 15:19:51 -0200

    Remove unused code in rails admin photos lib

--- lib/rails_admin_photos.rb
+++ lib/rails_admin_photos.rb
@@ -4,7 +4,6 @@ require 'rails_admin/config/actions/base'
 module RailsAdminPhotos
 end
 
-
 module RailsAdmin
   module Config
     module Actions
@@ -16,7 +15,7 @@ module RailsAdmin
         end
 
         register_instance_option :http_methods do
-          [:get, :put]
+          [:get]
         end
 
         register_instance_option :authorized? do
@@ -25,42 +24,10 @@ module RailsAdmin
 
         register_instance_option :controller do
           Proc.new do
-
-            if request.get? # EDIT
-
-              respond_to do |format|
-                format.html { render @action.template_name }
-                format.js   { render @action.template_name, :layout => false }
-              end
-
-            elsif request.put? # UPDATE
-
-
-              #@cached_assocations_hash = associations_hash
-              #@modified_assoc = []
-
-              #@old_object = @object.dup
-
-              #satisfy_strong_params!
-              #sanitize_params_for!(request.xhr? ? :modal : :update)
-
-              #@object.set_attributes(params[@abstract_model.param_key], _attr_accessible_role)
-              #@authorization_adapter && @authorization_adapter.attributes_for(:update, @abstract_model).each do |name, value|
-                #@object.send("#{name}=", value)
-              #end
-
-              #if @object.save
-                #@auditing_adapter && @auditing_adapter.update_object(@abstract_model, @object, @cached_assocations_hash, associations_hash, @modified_assoc, @old_object, _current_user)
-                #respond_to do |format|
-                  #format.html { redirect_to_on_success }
-                  #format.js { render :json => { :id => @object.id.to_s, :label => @model_config.with(:object => @object).object_label } }
-                #end
-              #else
-                #handle_save_error :edit
-              #end
-
+            respond_to do |format|
+              format.html { render @action.template_name }
+              format.js   { render @action.template_name, :layout => false }
             end
-
           end
         end
 
