ae12c9f4dc5081bd89fa51fab95adba671a51aa1
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-12-06 13:09:38 -0200

    Creates category and pages for links in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
+# Encoding: utf-8
 class PagesController < ApplicationController
   inherit_resources
   actions :show
 
   def index
+     @nursery = Page.where(category_id: Category.where(name: "Berçário e Minimaternal")).first rescue Page.new
+     @maternal = Page.where(category_id: Category.where(name: "Maternal")).first rescue Page.new
+     @garden = Page.where(category_id: Category.where(name: "Jardim")).first rescue Page.new
+     @tour = Page.where(category_id: Category.where(name: "Faça um tour virtual")).first rescue Page.new
+     @visit = Page.where(category_id: Category.where(name: "Visite a Baby House")).first rescue Page.new
+     @enroll = Page.where(category_id: Category.where(name: "Matricule seu filho")).first rescue Page.new
   end
 end
