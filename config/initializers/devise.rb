72b6e8b0e3d39cc245e38764afc17a09d499fdc2
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-12-07 15:13:50 -0200

    Trying something

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = I18n.t('site.emails.system')
+  config.mailer_sender = "system@escolababyhouse.com.br"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
